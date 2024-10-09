68092e220a0c56de6cb4b84571cc677e84d6f129
1HIM <jlassiayoub800@gmail.com>
2024-10-09 14:53:26 +0100

    first commit

---
+++ code 1.tsx
@@ -0,0 +1,12 @@
+//code 1:
+import React, { FC } from 'react';
+
+interface GreetingProps {
+  name: string;
+}
+
+const Greeting: FC<GreetingProps> = ({ name }) => {
+  return <div>Hello, {name}!</div>;
+}
+
+export default Greeting;
